494bb0af31f91596baa88dc70756351f5c693d5c
FernandoFrancaDev <soufadofernando@gmail.com>
2024-05-03 13:10:08 -0300

    análise de tipos de variáveis e constantaes

---
+++ tipos-variaveis/src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+      // estudartipos primitivos
+      // estudar a classe String que reprenta texto na aplicação
+      double salarioMinimo = 2500;
+
+      short numeroCurto = 1;
+      int numeroNormal = numeroCurto;
+      short numeroCurto2 = (short) numeroNormal;
+
+      int numero = 5;
+
+      numero = 10;
+
+      System.out.println(numero);
+
+     final double VALOR_DE_PI = 3.14;
+
+      
+
+    }
+}
