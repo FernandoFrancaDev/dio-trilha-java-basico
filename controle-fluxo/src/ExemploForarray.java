d0c691e113334002df8c5856c468658c8cfd3835
FernandoFrancaDev <soufadofernando@gmail.com>
2024-05-07 00:11:15 -0300

    atualização de novas classes

---
+++ controle-fluxo/src/ExemploForarray.java
@@ -0,0 +1,10 @@
+public class ExemploForarray {
+    public static void main(String[] args) {
+        String alunos[] = {"FELIPE", "JONAS", "JULIA", "MARCOS", "Carlos"};
+
+        for (String aluno: alunos){
+            System.out.println("Nome do aluno é: " + aluno);
+
+        }
+    }
+}
